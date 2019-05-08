477340c9f26252b808ae06f89190fae80f13e367
shubham chitranshi <shubhamchitranshi@shubhams-mbp.home>
2019-05-08 09:03:01 +0100

    Added Singleton pattern

---
+++ src/com/designpatterns/singleton/Singleton.java
@@ -0,0 +1,35 @@
+package com.designpatterns.singleton;
+
+/**
+ * This is an example of a singleton class
+ * @author shubhamchitranshi
+ *
+ */
+public class Singleton {
+	/*
+	 * the idea is to make the constructor private. This will 
+	 * restrict any other class to instantiate the object.
+	 * and provide static methods to get the instance.
+	 */
+	
+	private static Singleton instance;
+	private String name;
+	
+	private Singleton() {
+		
+	}
+	
+	public static Singleton getInstance() {
+		if(null == instance) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+	
+	public String toString() {
+		// just an example of other methods.
+		// these methods can contain business logic as normal.
+		return instance.name;
+	}
+
+}
